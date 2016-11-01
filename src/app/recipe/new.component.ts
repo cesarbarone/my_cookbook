ac280c1568a71f26940b29a9f0f1cd75dfcf14f5
César Barone <cesar.bmcosta@gmail.com>
2016-11-01 13:36:45 -0200

    ingredients support

--- src/app/recipe/new.component.ts
+++ src/app/recipe/new.component.ts
@@ -10,21 +10,46 @@ import { AngularFire, FirebaseListObservable, AuthProviders, AuthMethods } from 
 export class NewComponent {
 
   recipes: FirebaseListObservable<Recipe[]>;
-  recipe: Recipe = {
-    name: ''
-  };
+  recipe: Recipe;
+  ingredient: Ingredient;
   af: AngularFire;
 
   constructor(af: AngularFire) {
     this.af = af;
+    this.recipe = new Recipe('', '', new Array<Ingredient>());
+    this.ingredient = new Ingredient('', 0);
     this.recipes = this.af.database.list('/recipes');
   };
 
   createRecipe() {
     this.recipes.push(this.recipe);
   }
+
+  addIngredient(ingredient) {
+    var i = new Ingredient(ingredient.name, ingredient.quantity)
+    this.recipe.ingredients.push(i)
+    console.log(this.recipe)
+  }
 }
 
 export class Recipe {
   name: string;
+  method: string;
+  ingredients: Array<Ingredient>;
+
+  constructor(name: string, method: string, ingredients: Array<Ingredient>) {
+    this.name = name;
+    this.method = method;
+    this.ingredients = ingredients;
+  }
+}
+
+export class Ingredient {
+  name: string;
+  quantity: number;
+
+  constructor(name:string, quantity:number) {
+    this.name = name;
+    this.quantity = quantity;
+  }
 }
