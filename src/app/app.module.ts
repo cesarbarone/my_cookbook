fc89a6c04b4edfeb0b101cdb6fe6e1f8dedef498
César Barone <cesar.bmcosta@gmail.com>
2017-01-11 09:20:41 -0200

    removing keys

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,11 @@ import { MenuModule } from './menu/menu.module';
 import { AppComponent } from './app.component';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCL0LhoHZ_nnzmj1FCJEogw43jQR3w8JUs",
-  authDomain: "my-cookbook-8a2d2.firebaseapp.com",
-  databaseURL: "https://my-cookbook-8a2d2.firebaseio.com",
-  storageBucket: "my-cookbook-8a2d2.appspot.com",
-  messagingSenderId: "912964350150"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 
 const myFirebaseAuthConfig = {
